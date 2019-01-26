d1d51fb3cbe72f8150007e2d46cb78e1c3a94c72
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2019-01-26 14:32:58 +0100

    form add new client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ListClientsComponent } from './components/list-clients/list-clients.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddClientComponent } from './components/add-client/add-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ListClientsComponent },
+  { path: "add", component: AddClientComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
